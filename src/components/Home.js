51068383b0b793f6c78f34e8503c9948a92bc3ab
jamesaudry <james.audry@gmail.com>
2022-03-02 22:27:32 -0500

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <>
+      <div className="home-banner">
+        <h1>Welcome to Stranger's Things</h1>
+      </div>
+      </>
+    )
+  }
+  
+  export default Home;
